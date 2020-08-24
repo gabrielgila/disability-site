2042f31c7609ef193e2618dd8bd2ca9c3412acec
mgila <marianogila@gmail.com>
2020-08-24 16:05:39 -0700

    added recom, useful data

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 function About(props) {
     return(
         <div className="row justify-content-center">
-            <div>
-                <div className="col-sm-12 col-md-6">
-                    <h1>asd</h1>
+            <Fade left>
+                <div className="data-header col-12">
+                    <h3 className="data-title">RECOMENDACIONES ÚTILES</h3>
                 </div>
-                <div className="col-sm-12 col-md-6">
-                    <p>bla bla bla bla bla</p>
+                <div className="container justify-content-center">
+                    <h4 className="data-place">ACERCA DE</h4>
+                    <hr />
+                    <p className="container paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Donec massa sapien faucibus et molestie ac feugiat. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Vestibulum lorem sed risus ultricies tristique. Sit amet porttitor eget dolor morbi non arcu. Viverra nam libero justo laoreet. Lectus urna duis convallis convallis. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Est velit egestas dui id ornare arcu odio ut sem. Augue eget arcu dictum varius duis at consectetur lorem. Ipsum a arcu cursus vitae congue. Mattis enim ut tellus elementum sagittis. Mollis aliquam ut porttitor leo a. Viverra nibh cras pulvinar mattis
+                    </p>
+
+                    <hr />
                 </div>
-            </div>
+            </Fade>
         </div>
     );
 }
--- src/components/DataComponent.js
+++ src/components/DataComponent.js
@@ -1,156 +1,238 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 function Data(props) {
     return(
         <div className="justify-content-center">
-            <div className="col-0 ">
-                <div>
-                    <div className="data-header col-12">
-                        <h3 className="data-title">ORGANISMOS, DATOS Y DIRECCIONES ÚTILES</h3>
-                    </div>
-                    <div className="col-12">
-                        <h4 className="data-place">SECRETARÍA DE EXTENSIÓN</h4>
-                        <hr />
-                        <h5 className="data-info">Coordinación Comisión
-                            sobre Discapacidad,
-                            asesoramiento y gestión.
-                        </h5>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Fabián Melo</h5>
-                        <h5 className="data-contact">
-                            Yrigoyen 662 1er piso o. 2,
-                            Tel. 0249 4444555 int. 1555
-                        </h5>
-                        <hr />
-                    </div>
+            <Fade left>
+                <div className="col-0 ">
+                    <div>
+                        <div className="data-header col-12">
+                            <h3 className="data-title">ORGANISMOS, DATOS Y DIRECCIONES ÚTILES</h3>
+                        </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12 data-place">SECRETARÍA DE EXTENSIÓN</div>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="container justify-content-center">
+                                <hr />
+                                <h5 className="ref-heavy">Referente Fabián Melo</h5>
+                                <h5 className="data-contact">
+                                    Yrigoyen 662 1er piso o. 2,
+                                    Tel. 0249 4444555 int. 1555
+                                </h5>
+                                <hr />
+                            </div>
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">SECRETARÍA DE
-                            BIENESTAR ESTUDIANTIL</h4>
-                        <hr />
-                        <h5 className="data-info" >Departamento de Trabajo
-                            Social, De. de Salud,
-                            Programa de Becas
-                            Residencias Universitarias.
-                        </h5>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Sandra Pantusa</h5>
-                        <h5 className="data-contact">
-                            Gimnasio Campus
-                            Universitario Tandil,
-                            Tel. 0249 4385510
-                        </h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12 data-place">SECRETARÍA DE
+                                    BIENESTAR ESTUDIANTIL</div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="data-info" >Departamento de Trabajo
+                                Social, De. de Salud,
+                                Programa de Becas
+                                Residencias Universitarias.
+                            </h5>
+                            <hr />
+                            <h5 className="ref-heavy">Referente Sandra Pantusa</h5>
+                            <h5 className="data-contact">
+                                Gimnasio Campus
+                                Universitario Tandil,
+                                Tel. 0249 4385510
+                            </h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">SECRETARÍA ACADÉMICA</h4>
-                        <hr />
-                        <h5 className="data-info">Asesoramiento
-                        </h5>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Laura Iglesias</h5>
-                        <h5 className="data-contact">
-                            Pinto 399, 2do piso Rectorado.
-                            Tel. 0249 4422000 Int. 118
-                        </h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12 data-place">SECRETARÍA ACADÉMICA</div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="data-info">Asesoramiento
+                            </h5>
+                            <hr />
+                            <h5 className="ref-heavy">Referente Laura Iglesias</h5>
+                            <h5 className="data-contact">
+                                Pinto 399, 2do piso Rectorado.
+                                Tel. 0249 4422000 Int. 118
+                            </h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-facultad">FACULTADES</h4>
-                    </div>
-                    <hr />
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-facultad">FACULTADES</h4>
+                                </div>
+                            </div>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">AGRONOMÍA</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Laura Aguas</h5>
-                        <h5 className="data-contact">Tel. 02281 433291 / 433292 / 433293</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">AGRONOMÍA</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Laura Aguas</h5>
+                            <h5 className="data-contact">Tel. 02281 433291 / 433292 / 433293</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">ARTE</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Carolina Cesario</h5>
-                        <h5 className="data-contact">Tel. 0249 4440631</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">ARTE</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Carolina Cesario</h5>
+                            <h5 className="data-contact">Tel. 0249 4440631</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">ECONÓMICAS</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Moisés Bueno</h5>
-                        <h5 className="data-contact">Tel. 0249 4385550</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">ECONÓMICAS</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Moisés Bueno</h5>
+                            <h5 className="data-contact">Tel. 0249 4385550</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">EXACTAS</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Mariel Contreras</h5>
-                        <h5 className="data-contact">Tel. 0249 4385650</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">EXACTAS</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Mariel Contreras</h5>
+                            <h5 className="data-contact">Tel. 0249 4385650</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">DERECHO</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Esteban Marmeto</h5>
-                        <h5 className="data-contact">Tel. 02281 425353</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">DERECHO</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Esteban Marmeto</h5>
+                            <h5 className="data-contact">Tel. 02281 425353</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">HUMANAS</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Graciela Sánchez</h5>
-                        <h5 className="data-contact">Tel. 0249 4385750</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">HUMANAS</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Graciela Sánchez</h5>
+                            <h5 className="data-contact">Tel. 0249 4385750</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">INGENIERÍA</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Néstor Ferreira</h5>
-                        <h5 className="data-contact">Tel. 02284 451055</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">INGENIERÍA</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Néstor Ferreira</h5>
+                            <h5 className="data-contact">Tel. 02284 451055</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">SALUD</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Nancy Cardoso</h5>
-                        <h5 className="data-contact">Tel. 02284 426382</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">SALUD</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Nancy Cardoso</h5>
+                            <h5 className="data-contact">Tel. 02284 426382</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">SOCIALES</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Gabriela Delia</h5>
-                        <h5 className="data-contact">Tel. 02284 45.0331</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">SOCIALES</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Gabriela Delia</h5>
+                            <h5 className="data-contact">Tel. 02284 45.0331</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">VETERINARIAS</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente Claudia Pingitore</h5>
-                        <h5 className="data-contact">Tel. 0249 4385850</h5>
-                        <hr />
-                    </div>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">VETERINARIAS</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente Claudia Pingitore</h5>
+                            <h5 className="data-contact">Tel. 0249 4385850</h5>
+                            <hr />
+                        </div>
 
-                    <div className="col-12">
-                        <h4 className="data-place">UNIDAD DE ENSEÑANZA QUEQUÉN</h4>
-                        <hr />
-                        <h5 className="ref-heavy">Referente María José Arena</h5>
-                        <h5 className="data-contact">Tel. 02262 453232</h5>
-                        <hr />
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-10 col-sm-12">
+                                    <h4 className="data-place">UNIDAD DE ENSEÑANZA QUEQUÉN</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container justify-content-center">
+                            <hr />
+                            <h5 className="ref-heavy">Referente María José Arena</h5>
+                            <h5 className="data-contact">Tel. 02262 453232</h5>
+                            <hr />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
         </div>
 
     );
