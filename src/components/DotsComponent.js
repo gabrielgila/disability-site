f0fb2ed66bcfd6bb092c95ff64649e156c621470
mgila <marianogila@gmail.com>
2020-08-31 16:27:09 -0700

    tootltips added on dots

--- src/components/DotsComponent.js
+++ src/components/DotsComponent.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactTooltip from "react-tooltip";
+import {NavLink} from "reactstrap";
 
 function Dots(props) {
 
@@ -24,11 +26,16 @@ function Dots(props) {
 
     return (
         <div className="paginationDots" onClick={() => console.log(props)}>
-            <span style={selected === 1 ? dotsel : dot}></span>
-            <span style={selected === 2 ? dotsel : dot}></span>
-            <span style={selected === 3 ? dotsel : dot}></span>
-            <span style={selected === 4 ? dotsel : dot}></span>
-            <span style={selected === 5 ? dotsel : dot}></span>
+            <span data-toggle="tooltip" data-tip data-for="home" style={selected === 1 ? dotsel : dot}></span>
+            <ReactTooltip id="home" place="top" effect="solid">Inicio</ReactTooltip>
+            <span data-toggle="tooltip" data-tip data-for="hear" style={selected === 2 ? dotsel : dot}></span>
+            <ReactTooltip id="hear" place="top" effect="solid">Discapacidad Auditiva</ReactTooltip>
+            <span data-toggle="tooltip" data-tip data-for="vision" style={selected === 3 ? dotsel : dot}></span>
+            <ReactTooltip id="vision" place="top" effect="solid">Discapacidad Visual</ReactTooltip>
+            <span data-toggle="tooltip" data-tip data-for="physics" style={selected === 4 ? dotsel : dot}></span>
+            <ReactTooltip id="physics" place="top" effect="solid">Discapacidad Físico/Motora</ReactTooltip>
+            <span data-toggle="tooltip" data-tip data-for="visceral" style={selected === 5 ? dotsel : dot}></span>
+            <ReactTooltip id="visceral" place="top" effect="solid">Discapacidad Visceral</ReactTooltip>
         </div >
     );
 }
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { ReactComponent as Logo } from '../logo/Unicen_logo2.svg';
+import ReactTooltip from "react-tooltip";
 
 const Header = (props) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,13 +17,22 @@ const Header = (props) => {
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="/inicio" data-toggle="tooltip" data-placement="top" title="Inicio">Inicio</NavLink>
+                            <NavLink href="/inicio" data-tip data-for="home">Inicio</NavLink>
+                            <ReactTooltip id="home" place="bottom" effect="solid">
+                                Inicio
+                            </ReactTooltip>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/acercade" data-toggle="tooltip" data-placement="top" title="Acerca de esta Guía">Acerca de esta Guía</NavLink>
+                            <NavLink href="/acercade" data-tip data-for="about">Acerca de esta Guía</NavLink>
+                            <ReactTooltip id="about" place="bottom" effect="solid">
+                                Acerca de esta Guía
+                            </ReactTooltip>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/datosutiles" data-toggle="tooltip" data-placement="top" title="Datos útiles">Datos útiles</NavLink>
+                            <NavLink href="/datosutiles" data-tip data-for="data">Datos útiles</NavLink>
+                            <ReactTooltip id="data" place="bottom" effect="solid">
+                                Datos útiles
+                            </ReactTooltip>
                         </NavItem>
                     </Nav>
                 </Collapse>
